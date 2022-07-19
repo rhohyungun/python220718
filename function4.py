84bc09f7f5329a21fff1cbb8aad705b04724f5c8
rhohyungun <omega5m@naver.com>
2022-07-19 13:27:40 +0900

    5장소스 업데이트

--- function4.py
+++ function4.py
@@ -16,3 +16,22 @@ def union(*ar):
 
 print(union("ham","egg"))
 print(union("ham","egg","spam"))
+
+
+
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL +=key + "=" + user[key] + "&"
+    return strURL
+
+print(userURIBuilder("credu.com", "80", id="kim", passwd="1234",
+name="mike"))
+#람다함수
+g=lambda x,y:x*y
+print(g(3,4))
+print((lambda x:x*x)(3))
+
+
+
+print( globals())
